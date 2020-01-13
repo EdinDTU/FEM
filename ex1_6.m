982e863d790009b5c32f5cdb28b9d66169ca3c91
EdinDTU <edini96@gmail.com>
2020-01-13 13:23:58 +0100

    hmm
    Ved ikke hvad der er sket her

--- ex1_6.m
+++ ex1_6.m
@@ -11,41 +11,31 @@ error = abs(my_intergral(a, b) - my_trapez(a, b));
 %% 
 
 func = @(x) exp(-800*(x - 0.4)^2) + 0.25*exp(-40*(x - 0.8)^2);
-VX = [
-    0.0;
-    0.25;
-    0.75
-    1.0
-    ];
 
-
-EToV = [
-    1, 2;
-    2, 3;
-    3, 4;
-    ];
 %%
 compute_error_decrease(func, VX, EToV)
 
 
 %%
+VX = [    0.0;    0.25;    0.75;    1.0    ]; 
+
+EToV = [    1, 2;    2, 3;    3, 4;    ];
+
 idxMarked = [1,3];
 
-EToVfine = zeros(size(EToV,1) + length(idxMarked), 2)
-VXfine = zeros(1, size(VX,1) +  length(idxMarked))
-
+EToVfine = zeros(size(EToV,1) + length(idxMarked), 2);
+VXfine = zeros(1, size(VX,1) +  length(idxMarked));
 
 EToVfine(:,1) = 1:size(EToVfine,1);
 VXfine(:,1) = 1:size(VXfine,1);
 
 for i = 1:length(VXfine)
-    if 
+    
 end
 
 
 for i = idxMarked
-    x1 = VX( EToV(i,1) )
-    x2 = VX( EToV(i,2) )
-    xc = x1 + (x2 - x1)/2
-    
+    x1 = VX( EToV(i,1));
+    x2 = VX( EToV(i,2));
+    xc = x1 + (x2 - x1)/2;
 end
